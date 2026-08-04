d4f6d2bfac302ef6c17e2769c55e9bfb18eb69d0
aunifthh <auniauzi@gmail.com>
2026-08-04 14:56:03 +0800

    Initial Commit

---
+++ SnapNutri/SnapNutriApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapNutriApp.swift
+//  SnapNutri
+//
+//  Created by auni fatihah auzi on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct SnapNutriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
