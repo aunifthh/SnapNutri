45d6e4c137810051513bfb7118f27fd8612e30eb
Mitoo <waisuku400@gmail.com>
2026-08-05 11:23:18 +0800

    login create

---
+++ SnapNutri/Login.swift
@@ -0,0 +1,21 @@
+//
+//  Login.swift
+//  SnapNutri
+//
+//  Created by STDCX_MACBOOK PRO 38 on 05/08/2026.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text("Hello, World!")
+        Text("Hello, gaiS!")
+
+        
+    }
+}
+
+#Preview {
+    Login()
+}
